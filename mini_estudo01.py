4f0f9c1f0ca059859aa61c5f9c09a5de83eee0ca
romulo-torres <romulotorres14@gmail.com>
2026-05-10 11:11:57 -0400

    agora com o sobel e o laplacian certos

--- mini_estudo01.py
+++ mini_estudo01.py
@@ -4,10 +4,17 @@ import cv2
 exp = Experimento()
 exp.preparar_dataset()
 
+def sobel_completo(img, ksize=3):
+    gray  = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    sobel_x = cv2.Sobel(gray, cv2.CV_64F, dx=1, dy=0, ksize=ksize)
+    sobel_y = cv2.Sobel(gray, cv2.CV_64F, dx=0, dy=1, ksize=ksize)
+    magnitude = cv2.magnitude(sobel_x, sobel_y)
+    return cv2.convertScaleAbs(magnitude)
+
 filtros = {
     "Sobel": {
-        "func": cv2.Sobel,
-        "params": {"ddepth": cv2.CV_8U, "dx": 1, "dy": 0, "ksize": 3}
+        "func": sobel_completo,   # função auxiliar abaixo
+        "params": {"ksize": 3}
     },
     "Laplacian": {
         "func": cv2.Laplacian,
